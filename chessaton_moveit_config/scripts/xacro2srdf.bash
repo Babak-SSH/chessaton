c6d0729853dac2cb65bf843f1aae74be087be6ea
Babak-SSH <babak.sefidgar@gmail.com>
2023-08-22 20:26:47 +0330

    Add: add script to convert xacrto of robot description to srdf that gazebo understands

---
+++ chessaton_moveit_config/scripts/xacro2srdf.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This script converts xacro (SRDF variant) into SRDF for `panda_description` package
+
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
+XACRO_PATH="$(dirname "${SCRIPT_DIR}")/srdf/chessaton.srdf.xacro"
+SRDF_PATH="$(dirname "${SCRIPT_DIR}")/srdf/chessaton.srdf"
+
+# Arguments for xacro
+XACRO_ARGS=(
+    name:=chessaton
+)
+
+# Remove old SRDF file
+rm "${SRDF_PATH}" 2>/dev/null
+
+# Process xacro into SRDF
+xacro "${XACRO_PATH}" "${XACRO_ARGS[@]}" -o "${SRDF_PATH}" &&
+echo "Created new ${SRDF_PATH}"
