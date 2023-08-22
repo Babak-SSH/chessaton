eea10bd60a903bb112b834479ff5f7f342e6c73d
Babak-SSH <babak.sefidgar@gmail.com>
2023-08-22 14:46:46 +0330

    Add: add scripts to convert xacro descriptor of robot to srdf or pure urdf

---
+++ chessaton_description/scripts/xacro2sdf.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This script converts xacro (URDF variant) into SDF for `panda_description` package
+
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
+XACRO_PATH="$(dirname "${SCRIPT_DIR}")/urdf/chessaton.urdf.xacro"
+SDF_PATH="$(dirname "${SCRIPT_DIR}")/chessaton/model.sdf"
+
+# Arguments for xacro
+XACRO_ARGS=(
+    name:=chessaton
+    ros2_control:=true
+    ros2_control_plugin:=gz
+    ros2_control_command_interface:=effort
+    gazebo_preserve_fixed_joint:=false
+)
+
+# Remove old SDF file
+rm "${SDF_PATH}" 2>/dev/null
+
+# Process xacro into URDF, then convert URDF to SDF and edit the SDF to use relative paths for meshes
+"${SCRIPT_DIR}/xacro2sdf_direct.bash" "${XACRO_PATH}" "${XACRO_ARGS[@]}" "${@:1}" >"${SDF_PATH}" &&
+echo "Created new ${SDF_PATH}"
---
+++ chessaton_description/scripts/xacro2urdf.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# This script converts xacro (URDF variant) into URDF for `panda_description` package
+
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
+XACRO_PATH="$(dirname "${SCRIPT_DIR}")/urdf/chessaton.urdf.xacro"
+URDF_PATH="$(dirname "${SCRIPT_DIR}")/urdf/chessaton.urdf"
+
+# Arguments for xacro
+XACRO_ARGS=(
+    name:=chessaton
+    gripper:=true
+    collision_arm:=true
+    collision_gripper:=true
+    ros2_control:=true
+    ros2_control_plugin:=gz
+    ros2_control_command_interface:=position
+    gazebo_preserve_fixed_joint:=false
+)
+
+# Remove old URDF file
+rm "${URDF_PATH}" 2>/dev/null
+
+# Process xacro into URDF
+xacro "${XACRO_PATH}" "${XACRO_ARGS[@]}" -o "${URDF_PATH}" &&
+echo "Created new ${URDF_PATH}"
