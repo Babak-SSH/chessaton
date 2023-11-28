2ead82b7478db208181ec0a82ee2cf54fa2c9f14
Babak-SSH <babak.sefidgar@gmail.com>
2023-11-28 17:13:40 +0330

    Modify: rename

--- chessaton_control/src/mtc_pick_place_demo.cpp
+++ chessaton_control/src/mtc_pick_place_demo.cpp
@@ -5,14 +5,14 @@
 #include <moveit/task_constructor/solvers.h>
 #include <moveit/task_constructor/stages.h>
 #if __has_include(<tf2_geometry_msgs/tf2_geometry_msgs.hpp>)
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+    #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #else
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+    #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #endif
 #if __has_include(<tf2_eigen/tf2_eigen.hpp>)
-#include <tf2_eigen/tf2_eigen.hpp>
+    #include <tf2_eigen/tf2_eigen.hpp>
 #else
-#include <tf2_eigen/tf2_eigen.h>
+    #include <tf2_eigen/tf2_eigen.h>
 #endif
 
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("mtc_tutorial");
