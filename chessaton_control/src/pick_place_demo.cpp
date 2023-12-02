938525f3e98d6c8dcb65b0fc9c857cd7a7bbdf7c
Babak-SSH <babak.sefidgar@gmail.com>
2023-12-03 00:45:53 +0330

    Modify: tuned pick and place demo by resizing object and table and fixing plan poses and fixing the launch file

--- chessaton_control/src/pick_place_demo.cpp
+++ chessaton_control/src/pick_place_demo.cpp
@@ -17,20 +17,34 @@ int main(int argc, char * argv[]) {
     std::thread([&executor]() { executor.spin(); }).detach();
 
     Chessaton::ActionHandler robot_mp(node);
-    double max_gripper = 0.03; 
-    // Chessaton::Position box_position(0.18, 0, 0.04);
+
+    // geometry_msgs::msg::Point grasp_position;
+    // grasp_position.x = 0.18;
+    // grasp_position.y = 0;
+    // grasp_position.z = 0.04;
     geometry_msgs::msg::Pose grasp_pose;
-    grasp_pose.orientation.z = 0;
-    grasp_pose.position.x = 0.18;
+    grasp_pose.orientation.w = 1;
+    grasp_pose.position.x = 0.205;
     grasp_pose.position.y = 0;
     grasp_pose.position.z = 0.04;
-    // Chessaton::Position goal_position(0.19, 0.08, 0.04);
-    // Chessaton::Position goal_position(0, 0.25, 0.24);
+    geometry_msgs::msg::Pose lift_pose = grasp_pose;
+    lift_pose.position.z += 0.1;
+    lift_pose.position.x -= 0.05;
+
+    // geometry_msgs::msg::Point goal_position;
+    // goal_position.x = 0.18;
+    // goal_position.y = 0;
+    // goal_position.z = 0.04;
     geometry_msgs::msg::Pose goal_pose;
-    goal_pose.orientation.z = 0;
-    goal_pose.position.x = 0.18;
-    goal_pose.position.y = 0;
-    goal_pose.position.z = 0.04;
+    goal_pose.orientation.w = 1;
+    goal_pose.position.x = 0.01;
+    goal_pose.position.y = 0.22;
+    goal_pose.position.z = 0.20;
+    geometry_msgs::msg::Pose above_goal_pose = goal_pose;
+    above_goal_pose.position.z += 0.05;
+    geometry_msgs::msg::Pose go_back_pose = goal_pose;
+    go_back_pose.position.y -= 0.08;
+
 
 
     // define a collision box
@@ -41,24 +55,24 @@ int main(int argc, char * argv[]) {
     primitive.dimensions.resize(3);
     primitive.dimensions[0] = 0.2;
     primitive.dimensions[1] = 0.2;
-    primitive.dimensions[2] = 0.2;
+    primitive.dimensions[2] = 0.15;
 
     geometry_msgs::msg::Pose box_pose;
     box_pose.orientation.w = 1.0;
     box_pose.position.x = 0.0;
     box_pose.position.y = 0.3;
-    box_pose.position.z = 0.1;
+    box_pose.position.z = 0.075;
 
     robot_mp.add_collision_obj(primitive, box_pose, object_id);
 
     // open gripper
-    robot_mp.move_gripper(0.03);
+    robot_mp.move_gripper("hand_open");
 
     // move tcp to box
     robot_mp.move_to(grasp_pose);
 
     // grasp the box
-    robot_mp.move_gripper(0.013);
+    robot_mp.move_gripper(0.014);
 
     // lift the box
     grasp_pose.position.z += 0.1;
@@ -66,14 +80,15 @@ int main(int argc, char * argv[]) {
     robot_mp.move_to(grasp_pose);
 
     // go to goal position
+    robot_mp.move_to(above_goal_pose);
     robot_mp.move_to(goal_pose);
 
     // open the gripper to release the box
-    robot_mp.move_gripper(0.03);
+    robot_mp.move_gripper("hand_open");
+    robot_mp.move_to(go_back_pose);
 
     // got to home position
-    robot_mp.move_to(grasp_pose);
-    // robot_mp.home();
+    robot_mp.home();
 
     // Shutdown ROS
     rclcpp::shutdown();
