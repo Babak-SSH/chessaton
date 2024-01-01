e6c4270396b8bc40e711ac3307e9005664f08b23
Babak-SSH <babak.sefidgar@gmail.com>
2024-01-02 01:31:29 +0330

    Add: pick and place demo with opencv

---
+++ chessaton_control/src/pick_place_opencv_demo.cpp
@@ -0,0 +1,25 @@
+#include "../include/action_handler.hpp"
+#include <unistd.h>
+
+int main(int argc, char * argv[]) {
+    // Initialize ROS and create the Node
+    rclcpp::init(argc, argv);
+    auto const node = std::make_shared<rclcpp::Node>(
+      "pick_place_demo",
+      rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
+    );
+    // we use sim_time for simulation in gazebo
+    rclcpp::Parameter sim_time_param("use_sim_time", true);
+    node->set_parameter(sim_time_param);
+
+    // We spin up a SingleThreadedExecutor for the current state monitor to get information about the robot's state.
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+    std::thread([&executor]() { executor.spin(); }).detach();
+
+    Chessaton::ActionHandler robot_mp(node);
+
+    // Shutdown ROS
+    rclcpp::shutdown();
+    return 0;
+}
