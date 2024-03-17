6e280128bbd862e14d48de203ad6729d14ff9d1d
Babak-SSH <babak.sefidgar@gmail.com>
2024-03-17 18:00:57 +0330

    Add: this program will activate the chess engine node and make instance of chess handler which will request a move from chess engine and move accordingly until one side mates.

--- chessaton_chess_manager/src/chess_robot.cpp
+++ chessaton_chess_manager/src/chess_robot.cpp
@@ -1,5 +1,14 @@
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
 #include "chess_handler.hpp"
 #include "position.hpp"
+#include "chessaton_interfaces/srv/set_state.hpp"
+
 
 int main(int argc, char * argv[]) {
     // Initialize ROS and create the Node
@@ -19,9 +28,38 @@ int main(int argc, char * argv[]) {
 
     Chessaton::ActionHandler robotMP(node);
 
-    Chessaton::ChessHandler robotCH(robotMP);
+    rclcpp::Client<chessaton_interfaces::srv::SetState>::SharedPtr client =
+      node->create_client<chessaton_interfaces::srv::SetState>("set_state");
 
-    robotCH.graspZ = 0.05;
-    robotCH.pick_piece(e1);
-    robotCH.put_piece(e4);    
+    auto request = std::make_shared<chessaton_interfaces::srv::SetState::Request>();
+    request->active = true;
+
+    while (!client->wait_for_service(std::chrono::seconds(1))) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+        return 0;
+      }
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    }
+
+    auto result = client->async_send_request(request);
+
+    Chessaton::ChessHandler robotCH(robotMP, START_FEN);
+    bool game = true;
+
+    while (game) {
+        std::string best_move = robotCH.get_engine_move(node, 11);
+        if (!best_move.find("end")) {
+            game = false;
+            best_move = best_move.substr(0, best_move.find(" end"));
+        }
+        robotCH.make_move(best_move);
+    }
+
+    request->active = false;
+    result = client->async_send_request(request);
+
+    // shutdown ROS
+    rclcpp::shutdown();
+    return 0;
 }
