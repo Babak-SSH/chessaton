4d8ee79674f2ba83f538f16372d99f6d5a01db59
Babak-SSH <babak.sefidgar@gmail.com>
2024-05-03 19:53:02 +0330

    Add: add the box to be picked as a refrence for octo environment so robot can pick the box in simulation. Modify: fixed some values.

--- chessaton_control/src/pick_place_opencv_demo.cpp
+++ chessaton_control/src/pick_place_opencv_demo.cpp
@@ -77,11 +77,17 @@ int main(int argc, char * argv[]) {
     geometry_msgs::msg::Pose grasp_pose;
     grasp_pose.orientation.w = 1;
     grasp_pose.position.x = (double)((int)(response.box_position.x*100))/100;
-    grasp_pose.position.y = (double)((int)(response.box_position.y*100))/100;
-    grasp_pose.position.z = (double)((int)(response.box_position.z*100))/100 * 0.8;
+    grasp_pose.position.y = ((double)((int)(response.box_position.y*100))/100) * 1.05;
+    grasp_pose.position.z = ((double)((int)(response.box_position.z*100))/100) * 0.8;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "grasp position x: %f", grasp_pose.position.x);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "grasp position y: %f", grasp_pose.position.y);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "grasp position z: %f", grasp_pose.position.z);
     geometry_msgs::msg::Pose lift_pose = grasp_pose;
     lift_pose.position.z += 0.1;
     lift_pose.position.y += 0.05;
+    geometry_msgs::msg::Pose approach_pose = grasp_pose;
+    approach_pose.position.y += 0.04;
+    approach_pose.position.z += 0.03;
 
     geometry_msgs::msg::Pose goal_pose;
     goal_pose.orientation.w = 1;
@@ -96,14 +102,38 @@ int main(int argc, char * argv[]) {
     geometry_msgs::msg::Pose retreat_pose = goal_pose;
     retreat_pose.position.y -= 0.08;
 
+    // define a collision box
+    std::string box_id = "blue_box";
+
+    shape_msgs::msg::SolidPrimitive box_primitive;
+    box_primitive.type = box_primitive.BOX;
+    box_primitive.dimensions.resize(3);
+    box_primitive.dimensions[0] = 0.028;
+    box_primitive.dimensions[1] = 0.028;
+    box_primitive.dimensions[2] = 0.054;
+
+    geometry_msgs::msg::Pose box_pose;
+    box_pose.orientation.w = 1.0;
+    box_pose.position.x = 0;
+    box_pose.position.y = -0.30;
+    box_pose.position.z = 0.045;
+
+    robot_mp.add_collision_obj(box_primitive, box_pose, box_id);
+    robot_mp.allow_collision(box_id, true);
+
     // open gripper
     robot_mp.move_gripper("hand_open");
+
+    // approach to box
+    robot_mp.move_to(approach_pose);
 
     // move tcp to box
     robot_mp.move_to(grasp_pose);
 
     // grasp the box
     robot_mp.move_gripper(0.014);
+
+    robot_mp.attach_obj(box_id);
 
     // lift the box
     robot_mp.move_to(lift_pose);
@@ -114,6 +144,10 @@ int main(int argc, char * argv[]) {
 
     // open the gripper to release the box
     robot_mp.move_gripper("hand_open");
+    robot_mp.detach_obj(box_id);
+    robot_mp.remove_obj(box_id);
+    robot_mp.move_to(retreat_pose);
+
     robot_mp.move_to(retreat_pose);
 
     // got to home position
